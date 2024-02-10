adbf1cc400e2b3a1b62ad0f5d856a8e3fc19354f
Eduardo Pinheiro <duedugaranito@gmail.com>
2024-02-10 12:14:56 -0300

    Initial Commit

---
+++ daily phrases/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  daily phrases
+//
+//  Created by Eduardo Pinheiro on 10/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
